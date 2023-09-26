6225c0dee2750256042f32d8b91330d1da942fa3
流浪的灵魂 <77929833@qq.com>
2023-09-26 10:56:43 +0800

    base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/lostarkguide/",
+  base: "/",
 
   lang: "zh-CN",
   title: "文档演示",
